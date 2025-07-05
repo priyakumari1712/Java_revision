bca214c9132bfd3467edd2857571337745ce94f7
priya <priyaagrwal858@gmail.com>
2025-07-05 13:33:45 +0530

    showding in java

---
+++ function/Showding.java
@@ -0,0 +1,8 @@
+public class Showding {
+    static int a = 20;
+    public static void main(String[] args) {
+        System.out.println(a);
+        int a = 40; // showding in line 5 
+        System.out.println(a);
+    }
+}
